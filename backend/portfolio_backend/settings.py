c010a5b849420333e6530e991cfb8aefcc4fb9a1
Mohammed Anjoom V <anjoomvkkl@gmail.com>
2026-01-07 15:24:12 +0530

    Enable Debug Mode and relax DB SSL

--- backend/portfolio_backend/settings.py
+++ backend/portfolio_backend/settings.py
@@ -31,77 +31,24 @@ SECRET_KEY = 'django-insecure-my5l^yb1%hh$$la)r-^y!0%h8bxhvbg&vcjh-i^+_%oqonkr*9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-
 ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'corsheaders',
-    'api',
-]
-
-MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-ROOT_URLCONF = 'portfolio_backend.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'portfolio_backend.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# ... (rest of config)
 
 # If we are on Vercel (or have a DATABASE_URL), use the cloud Postgres
 if os.environ.get('POSTGRES_URL'):
     DATABASES['default'] = dj_database_url.config(
         env='POSTGRES_URL',
         conn_max_age=600,
-        ssl_require=True
+        # ssl_require=True  <-- Removed to let connection string handle it
     )
 elif os.environ.get('DATABASE_URL'):
     DATABASES['default'] = dj_database_url.config(
         conn_max_age=600,
-        ssl_require=True
+        # ssl_require=True  <-- Removed
     )
 
 
